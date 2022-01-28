79e4bc15d5aaffb363f847df5ceda95ab0caeb7e
Ken Mauch <71083977+KenMauch3@users.noreply.github.com>
2022-01-27 21:46:45 -0500

    complete challenge

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setNote((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          placeholder="Title"
+          value={note.title}
+          onChange={handleChange}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={note.content}
+          onChange={handleChange}
+        />
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            props.addNote(note);
+            setNote({
+              title: "",
+              content: ""
+            });
+          }}
+        >
+          Add
+        </button>
       </form>
     </div>
   );
